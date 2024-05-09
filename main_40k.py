f73ff6d042c8516ae713d1855a0d5e3f55b29b77
thisisAdrian142 <EmailAdrian.png@gmail.com>
2024-05-09 18:37:48 +1000

    updated tasks

---
+++ main_40k.py
@@ -0,0 +1,11 @@
+## OBJECTIVE ##
+
+## Print the whole list of characters that appears in 40k novel ##
+
+## Functions structure ##
+
+## COUNT WORDS ##
+
+## COUNT LETTERS ##
+
+## PRINT A REPORT ##
