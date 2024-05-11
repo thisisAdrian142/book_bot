83b744f5d222eb8144bc733bb280aa507a2af3b4
thisisAdrian142 <EmailAdrian.png@gmail.com>
2024-05-12 04:47:55 +1000

    changed

--- main_40k.py
+++ main_40k.py
@@ -6,9 +6,10 @@
 
 # read the book
 def main():
-    book_path = 40k_books_bot/astrum_militarum.txt 
+    book_path = "new_books_bot/astrum_militarum.txt" 
     with open(book_path) as f:
         read_book = f.read()
         print(read_book)
 
 main()
+
